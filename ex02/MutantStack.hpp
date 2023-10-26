25b431300e4fb94f0a1202add27537a309fa49c9
Anouar <anouarelmaaroufi@gmail.com>
2023-10-26 13:54:49 +0100

    EX02 done
    Created a MutanStack class template, that will inherit from std::stack, and add iterators to it (begin and end)

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 18:36:38 by ael-maar          #+#    #+#             */
+/*   Updated: 2023/10/25 19:46:05 by ael-maar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
+{
+    public:
+        MutantStack() {}
+        MutantStack(const MutantStack &src) { *this = src; }
+        MutantStack &operator=(const MutantStack &src)
+        {
+            if (this != &src)
+                this->c = src.c;
+            return (*this);
+        }
+        ~MutantStack() {}
+        typedef typename Container::iterator iterator;
+        iterator begin() { return (this->c.begin()); }
+        iterator end() { return (this->c.end()); }
+};
+
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 18:36:40 by ael-maar          #+#    #+#             */
+/*   Updated: 2023/10/25 19:44:54 by ael-maar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+int main()
+{
+    MutantStack<int, std::vector<int> > mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    MutantStack<int, std::vector<int> >::iterator it = mstack.begin();
+    MutantStack<int, std::vector<int> >::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int, std::vector<int> > s(mstack);
+    return 0;
+}
